97ec02f365ac1e247d07e8893e8fa522771b7d32
RadiALo <78644882+RadiALo@users.noreply.github.com>
2024-10-10 14:28:35 +0300

    Articles and categories pages, hrefs

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ app.set('view engine', 'pug');
 app.set('views', './views');
 
 app.get('/', (req, res) => { res.render('index', { title: 'Blog app' }) })
+app.get('/categories', (req, res) => { res.render('categories', { title: 'Blog app'}) })
+app.get('/articles', (req, res) => { res.render('articles', { title: 'Blog app'}) })
 app.listen(3000, () => console.log('Server is running on port 3000!'));
